cc6fff4dbbe026f5a881aeeece9f9cd16482d96c
OskarCastorena <oskarcastorena@icloud.com>
2025-04-12 22:21:09 -0600

    Added a roles dictionary in a single py file

---
+++ game/roles.py
@@ -0,0 +1,12 @@
+# This archive contents the roles that a player could choose.
+roles = {'ROCKERBOYS': "Rock-and-roll rebels who use performance, art, and rhetoric to fight authority.",
+         'SOLOS': "Assassins, bodyguards, killers, and soldiers-for-hire in a lawless new world.",
+         'NETRUNNERS': "Cybernetic master hackers of the post-NET world and brain-burning secret stealers.",
+         'TECHS': "Renegade mechanics and supertech inventors; the people who make the Dark Future run.",
+         'MEDTECHS': "Unsanctioned street doctors and cyberware medics, patching up meat and metal alike.",
+         'MEDIAS': "Reporters, media stars, and social influencers risking it all for the truth—or glory.",
+         'EXECS': "Corporate power brokers and business raiders fighting to restore the rule of the Megacorps.",
+         'LAWMEN': "Maximum law enforcers patrolling the mean streets and barbarian warrior highways beyond.",
+         'FIXERS': "Dealmakers, organizers, and information brokers in the post-War Midnight Markets of The Street.",
+         'NOMADS': "Transport experts, ultimate road warriors, pirates, and smugglers who keep the world connected."}
+print(roles['ROCKERBOYS'])
